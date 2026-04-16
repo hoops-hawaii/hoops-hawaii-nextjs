0fb210ec9275fc7b3249cb0a100474e19f7d75fa
dex965 <143553406+dex965@users.noreply.github.com>
2026-04-15 21:10:05 -1000

    update

--- src/app/auth/signout/page.tsx
+++ src/app/auth/signout/page.tsx
@@ -5,9 +5,9 @@ import { Button, Col, Row } from 'react-bootstrap';
 
 /** After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
 const SignOut = () => (
-  <Col id="signout-page" className="text-center py-3">
-    <h2>Do you want to sign out?</h2>
-    <Row>
+  <Col id="signout-page" className="d-flex flex-column justify-content-center align-items-center text-center">
+    <h1 className = "text-light">Do you want to sign out?</h1>
+    <Row className=' justify-content-center w-100 p-4'>
       <Col xs={4} />
       <Col>
         <Button variant="danger" onClick={() => signOut({ callbackUrl: '/', redirect: true })}>
