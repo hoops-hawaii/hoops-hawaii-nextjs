8d5258200e5b333c8c6b52dcaa41b05e6acafe2d
dex965 <143553406+dex965@users.noreply.github.com>
2026-04-16 08:46:02 -1000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Nav, Row } from 'react-bootstrap';
 
 /** The Home page. */
 const Home = () => (
   <main>
     <Container id="landing-page" fluid className="vh-100">
-      <Row className="h-100 align-items-center justify-content-center">
+      <Row className="h-100 align-items-center justify-content-center ">
         <Col className="text-center text-white">
+          <Nav id = 'centerTextBox'className='text-center col-4 mx-auto p-0 w-full container-fluid justify-content-center'>
           <h1>FIND COURTS.</h1>
           <h1>FIND TEAMMATES.</h1>
           <h1>PLAY BASKETBALL.</h1>
+          </Nav>
         </Col>
       </Row>
     </Container>
