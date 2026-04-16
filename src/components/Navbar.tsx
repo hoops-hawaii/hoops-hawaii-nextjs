46a3413309bc825e4984b200427465cf0b3cead4
dex965 <143553406+dex965@users.noreply.github.com>
2026-04-16 08:52:59 -1000

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,8 @@
 import { useSession } from 'next-auth/react'; // v5 compatible
 import { usePathname } from 'next/navigation';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { Person, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
+import { BoxArrowRight, Lock, Person, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
+
 
 const NavBar: React.FC = () => {
   const { data: session, status } = useSession();
@@ -41,10 +42,16 @@ const NavBar: React.FC = () => {
           </Nav>
           <Nav>
             {session ? (
-              <Nav.Link id="profile-link" href="/profile/view" active={pathName === '/profile/view'}>
-                <Person />
-                {currentUser}
-              </Nav.Link>
+              <NavDropdown id="login-dropdown" title={currentUser}>
+                <NavDropdown.Item id="login-dropdown-sign-out" href="/api/auth/signout">
+                  <BoxArrowRight/>
+                  Sign Out
+                </NavDropdown.Item>
+                <NavDropdown.Item id="login-dropdown-change-password" href="/auth/change-password">
+                  <Lock />
+                  Change Password
+                </NavDropdown.Item>
+              </NavDropdown>
             ) : (
               <NavDropdown id="profile-dropdown" title="Profile">
                 <NavDropdown.Item id="profile-dropdown-sign-in" href="/auth/signin">
