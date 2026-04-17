cf18f6fff09d95e68a8bc23e1655f073a7a17837
dex965 <143553406+dex965@users.noreply.github.com>
2026-04-16 21:28:02 -1000

    update

--- src/app/auth/signout/page.tsx
+++ src/app/auth/signout/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { signOut } from 'next-auth/react'; // v5 compatible
-import { Button, Col, Row } from 'react-bootstrap';
+import { Button, Col, Row , Nav} from 'react-bootstrap';
 
 /** After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
 const SignOut = () => (
   <Col id="signout-page" className="d-flex flex-column justify-content-center align-items-center text-center">
+    <Nav id = 'centerTextBox'className='text-center col-4 mx-auto p-0 w-full container-fluid justify-content-center'>
     <h1 className = "text-light">Do you want to sign out?</h1>
+    </Nav>
     <Row className=' justify-content-center w-100 p-4'>
       <Col xs={4} />
       <Col>
