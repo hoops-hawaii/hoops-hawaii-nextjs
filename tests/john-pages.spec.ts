4528126b7abe9d428f3bd262a337848c56e6f9f2
jacobamaier <maier.jacobaugust@gmail.com>
2026-04-17 15:39:25 -1000

    potentially fixed playwright test to work

--- tests/john-pages.spec.ts
+++ tests/john-pages.spec.ts
@@ -1,17 +1,17 @@
 import { test, expect } from './auth-utils';
-/*
+
 test.slow();
 test('can authenticate a specific user', async ({ getUserPage }) => {
 
   // Call the getUserPage fixture with users signin info to get authenticated session for user
-  const customUserPage = await getUserPage('john@foo.com', 'changeme');
+  const customUserPage = await getUserPage('john', 'changeme');
 
   // Navigate to the home page and wait for post-login indicator
   await customUserPage.goto('http://localhost:3000/');
   await expect(
-    customUserPage.getByRole('button', { name: 'john@foo.com' })
+    customUserPage.getByRole('button', { name: 'john' })
   ).toBeVisible({ timeout: 10000 });
-
+/*
   // Now check for navigation links and headings
   await expect(
     customUserPage.getByRole('link', { name: 'Add Stuff' })
